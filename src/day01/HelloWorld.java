e93904db1569bfbcb2adc21e9f9c28daf3b75675
Zarina <zarina_malikova@yahoo.com>
2025-12-14 11:05:17 -0800

    Add additional greeting messages to HelloWorld

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -5,6 +5,8 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.println("Hello Cydeo");
+        System.out.println("Hello WProgrammers");
 
 
     }
